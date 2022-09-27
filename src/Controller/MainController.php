f120b3f3712b82b76b8cbdc16fa34b4071dc3ed1
Ryan Weaver <ryan@thatsquality.com>
2022-09-27 14:46:38 -0400

    adding dummy pages

---
+++ src/Controller/MainController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/', name: 'app_homepage')]
+    public function homepage()
+    {
+        return $this->render('main/homepage.html.twig');
+    }
+
+    #[Route('/recipes', name: 'app_recipes')]
+    public function recipes()
+    {
+        return $this->render('main/recipes.html.twig');
+    }
+
+    #[Route('/recipes/bake-cookies', name: 'app_recipes_show')]
+    public function recipesShow()
+    {
+        return $this->render('main/recipes_show.html.twig');
+    }
+
+    #[Route('/skills', name: 'app_skills')]
+    public function skills()
+    {
+        return $this->render('main/skills.html.twig');
+    }
+
+    #[Route('/skills/sharpen-knives', name: 'app_skills_show')]
+    public function skillsShow()
+    {
+        return $this->render('main/skills_show.html.twig');
+    }
+}
