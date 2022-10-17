79a60bc1d7925a712647f106d42caa999a6dab2d
Ryan Weaver <ryan@thatsquality.com>
2022-10-17 14:27:27 -0400

    Adding User fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Factory\RecipeFactory;
+use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -11,6 +12,11 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         RecipeFactory::createMany(25);
+
+        UserFactory::createOne([
+            'email' => 'doggo_admin@barkbite.com',
+            'password' => 'woof',
+        ]);
 
         $manager->flush();
     }
