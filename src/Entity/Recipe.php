4e44075ed703d0d3f6fc46967dc99a5d13748f57
Ryan Weaver <ryan@thatsquality.com>
2022-10-17 11:43:49 -0400

    Adding Recipe entity

---
+++ src/Entity/Recipe.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RecipeRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RecipeRepository::class)]
+class Recipe
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $subText = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $slug = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $imageFilename = null;
+
+    #[ORM\Column(type: Types::JSON)]
+    private array $ingredients = [];
+
+    #[ORM\Column(type: Types::JSON)]
+    private array $instructions = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSubText(): ?string
+    {
+        return $this->subText;
+    }
+
+    public function setSubText(string $subText): self
+    {
+        $this->subText = $subText;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getImageFilename(): ?string
+    {
+        return $this->imageFilename;
+    }
+
+    public function setImageFilename(string $imageFilename): self
+    {
+        $this->imageFilename = $imageFilename;
+
+        return $this;
+    }
+
+    public function getIngredients(): array
+    {
+        return $this->ingredients;
+    }
+
+    public function setIngredients(array $ingredients): self
+    {
+        $this->ingredients = $ingredients;
+
+        return $this;
+    }
+
+    public function getInstructions(): array
+    {
+        return $this->instructions;
+    }
+
+    public function setInstructions(array $instructions): self
+    {
+        $this->instructions = $instructions;
+
+        return $this;
+    }
+}
