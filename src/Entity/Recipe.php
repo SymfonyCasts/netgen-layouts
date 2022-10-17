3b547770d17ce86e7537407fae3c65bc515f8d40
Ryan Weaver <ryan@thatsquality.com>
2022-10-17 11:48:48 -0400

    Adding Timestampable and Sluggable

--- src/Entity/Recipe.php
+++ src/Entity/Recipe.php
@@ -5,10 +5,14 @@ namespace App\Entity;
 use App\Repository\RecipeRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation\Slug;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
 
 #[ORM\Entity(repositoryClass: RecipeRepository::class)]
 class Recipe
 {
+    use TimestampableEntity;
+
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
@@ -20,7 +24,8 @@ class Recipe
     #[ORM\Column(length: 255)]
     private ?string $subText = null;
 
-    #[ORM\Column(length: 100)]
+    #[ORM\Column(length: 100, unique: true)]
+    #[Slug(fields: ['name'])]
     private ?string $slug = null;
 
     #[ORM\Column(length: 255)]
