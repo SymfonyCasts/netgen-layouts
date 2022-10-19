40af7cf547d7a39603260093f720e1e690a6f67a
Ryan Weaver <ryan@thatsquality.com>
2022-10-19 14:45:19 -0400

    tuts hero working...

---
+++ src/ContentBrowser/BrowserRootLocation.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\ContentBrowser;
+
+use Netgen\ContentBrowser\Item\LocationInterface;
+
+class BrowserRootLocation implements LocationInterface
+{
+    public function getLocationId()
+    {
+        // TODO: Implement getLocationId() method.
+    }
+
+    public function getName(): string
+    {
+        // TODO: Implement getName() method.
+    }
+
+    public function getParentId()
+    {
+        // TODO: Implement getParentId() method.
+    }
+}
