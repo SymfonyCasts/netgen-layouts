dbf5080deb54b9c4d0a22f13b17f0d62aea90e50
Ryan Weaver <ryan@thatsquality.com>
2022-10-19 14:54:51 -0400

    tuts hero working...

--- src/ContentBrowser/RecipeBrowserBackend.php
+++ src/ContentBrowser/RecipeBrowserBackend.php
@@ -59,7 +59,11 @@ class RecipeBrowserBackend implements BackendInterface
 
     public function getSubItemsCount(LocationInterface $location): int
     {
-        return 0;
+        return $this->recipeRepository
+            ->createQueryBuilderOrderedByNewest()
+            ->select('COUNT(recipe.id)')
+            ->getQuery()
+            ->getSingleScalarResult();
     }
 
     public function search(string $searchText, int $offset = 0, int $limit = 25): iterable
