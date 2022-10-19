d9433aeb7959e9097c3dbbd254ac0d808bcac500
Ryan Weaver <ryan@thatsquality.com>
2022-10-19 14:57:03 -0400

    tuts hero working...

---
+++ src/Layouts/RecipeValueLoader.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Layouts;
+
+use Netgen\Layouts\Item\ValueLoaderInterface;
+
+class RecipeValueLoader implements ValueLoaderInterface
+{
+    public function load($id): ?object
+    {
+        // TODO: Implement load() method.
+    }
+
+    public function loadByRemoteId($remoteId): ?object
+    {
+        // TODO: Implement loadByRemoteId() method.
+    }
+}
