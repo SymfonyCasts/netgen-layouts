d3db146c1a01f660dcb12a04e4d7ef68f74aa1bf
Ryan Weaver <ryan@thatsquality.com>
2022-10-24 10:02:49 -0400

    tuts hero working...

--- src/Layouts/SkillSlugger.php
+++ src/Layouts/SkillSlugger.php
@@ -3,12 +3,15 @@
 namespace App\Layouts;
 
 use Netgen\Layouts\Contentful\Entity\ContentfulEntry;
+use Netgen\Layouts\Contentful\Routing\EntrySlugger\FilterSlugTrait;
 use Netgen\Layouts\Contentful\Routing\EntrySluggerInterface;
 
 class SkillSlugger implements EntrySluggerInterface
 {
+    use FilterSlugTrait;
+
     public function getSlug(ContentfulEntry $contentfulEntry): string
     {
-        // TODO: Implement getSlug() method.
+        return '/skills/'.$this->filterSlug($contentfulEntry->get('title'));
     }
 }
