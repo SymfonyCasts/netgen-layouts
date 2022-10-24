743c9f5edbff22a29edcf6331a3b346c50f1ad49
Ryan Weaver <ryan@thatsquality.com>
2022-10-24 10:16:28 -0400

    tuts hero working...

--- src/Layouts/ContentfulSlugger.php
+++ src/Layouts/ContentfulSlugger.php
@@ -5,7 +5,7 @@ namespace App\Layouts;
 use Netgen\Layouts\Contentful\Entity\ContentfulEntry;
 use Netgen\Layouts\Contentful\Routing\EntrySluggerInterface;
 
-class SkillSlugger implements EntrySluggerInterface
+class ContentfulSlugger implements EntrySluggerInterface
 {
     public function getSlug(ContentfulEntry $contentfulEntry): string
     {
