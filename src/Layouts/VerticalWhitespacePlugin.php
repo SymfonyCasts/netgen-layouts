eb8c64004aec7c491d34762f92fd865e502715f6
Ryan Weaver <ryan@thatsquality.com>
2022-10-24 15:18:42 -0400

    tuts hero working...

---
+++ src/Layouts/VerticalWhitespacePlugin.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Layouts;
+
+use Netgen\Layouts\Block\BlockDefinition\Handler\Plugin;
+
+class VerticalWhitespacePlugin extends Plugin
+{
+    public static function getExtendedHandlers(): iterable
+    {
+        // TODO: Implement getExtendedHandlers() method.
+    }
+
+
